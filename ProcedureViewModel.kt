94de1d5b5e48c41c692e60b5088671acbc114b44
NRamaniMrudula215f4 <136070194+nramanimrudula215f4@users.noreply.github.com>
2024-04-27 11:54:13 +0530

    Create ProcedureViewModel.kt

---
+++ ProcedureViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class CategoriesViewModel : ViewModel() {
+    //only for writing we are using private
+    private var _categories = MutableStateFlow<List<Category>>(emptyList())
+    val categories: StateFlow<List<Category>> = _categories
+
+    init {
+        fetchCategories()
+    }
+
+    private fun fetchCategories() {
+        viewModelScope.launch {
+            val repo = CategoryRepository()
+            val response = repo.getCategories()
+            _categories.value = response
+        }
+    }}
